dede27595c7c935bf0473d299fcf1e7f7f9128c0
William <william.costa@fattoriaweb.com.br>
2023-02-28 00:57:41 -0300

    banners - falta ajustes no swiper

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -1,15 +1,64 @@
-import React, {useState, useEffect} from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import React, { useState, useEffect, useRef } from 'react';
+import SwiperCore, { Autoplay, Navigation, Pagination } from 'swiper/core';
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
 import styles from './Banner.module.scss';
 
+SwiperCore.use([Autoplay, Navigation, Pagination]);
 
 export default function Banner() {
 
+    
+    const swiperParams = {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        pagination: {
+            clickable: true,
+            dynamicBullets: true,
+        },
+        loop: true,
+        autoplay: {
+            delay: 10000,
+            disableOnInteraction: true,
+        },
+        navigation: true,
+        className: styles.banner_swiper,
+        onSlideChange: () => console.log('slide change'),
+        onSwiper: (swiper) => console.log(swiper),
+    };
+
 
     return (
-       <section className={`${styles.banner_principal}`}>
-
-       </section>
+        <Swiper {...swiperParams}>
+            <SwiperSlide>
+                <div style={{ backgroundImage: `url("/images/banner-1.jpg")` }} className={`${styles.banner}`}>
+                    <a href="#" className={`${styles.btn_banner}`}>Travel</a>
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div style={{ backgroundImage: `url("/images/banner-2.jpg")` }} className={`${styles.banner}`}>
+                <a href="#" className={`${styles.btn_banner}`}>Travel</a>
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div style={{ backgroundImage: `url("/images/banner-3.jpg")` }} className={`${styles.banner}`}>
+                <a href="#" className={`${styles.btn_banner}`}>Travel</a>
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div style={{ backgroundImage: `url("/images/banner-4.jpg")` }} className={`${styles.banner}`}>
+                <a href="#" className={`${styles.btn_banner}`}>Travel</a>
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div style={{ backgroundImage: `url("/images/banner-5.jpg")` }} className={`${styles.banner}`}>
+                <a href="#" className={`${styles.btn_banner}`}>Travel</a>
+                </div>
+            </SwiperSlide>
+        </Swiper>
     )
-    
+
 }
 
